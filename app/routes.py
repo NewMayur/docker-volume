9f6fdf4b9bb9e42b7f040dedbd63b8ee540853a8
NewMayur <mayurgharat28@gmail.com>
2024-08-19 16:20:18 +0530

    Bing app

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask import current_app  # Add this import
+from app import db
+from app.models import Message
 
 bp = Blueprint('main', __name__)
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        message_content = request.form['content']
+        new_message = Message(content=message_content)
+        db.session.add(new_message)
+        db.session.commit()
+        return redirect(url_for('main.index'))  # Change 'bp.index' to 'main.index'
+
+    messages = Message.query.all()
+    return render_template('index.html', messages=messages)
